487963cadf680e1534964d42b699a7e59e2e6b87
Eko Yunianto <eyeyunianto@gmail.com>
2014-12-28 12:36:49 +0700

    Landing page

--- katana/js/app.js
+++ katana/js/app.js
@@ -4,5 +4,13 @@ App = Ember.Application.create({
 
 App.Router.map(function() {
   this.resource('countdown');
-  this.resource('home')
+  this.resource('home');
+  this.resource('howto',function(){
+  	this.route('featured');
+  	this.route('activity');
+  	this.route('feeds');
+  	this.route('prayer');
+  	this.route('qiblah');
+  	this.route('masjid');
+  })
 });
